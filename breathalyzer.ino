084730793e8a61d102e26f281d8c3103fd6b66ab
Драгомир Апостолов <107076863+Crepersub@users.noreply.github.com>
2025-06-24 06:26:26 +0300

    Finished version

--- breathalyzer.ino
+++ breathalyzer.ino
@@ -1,40 +1,64 @@
-#include <U8g2lib.h>
 
-#define csPin 10
-#define dcPin 8
-#define resPin 9
+#include <U8g2lib.h>
 
 #define alcoPin 0
 
 #define buttonPin 6
 #define buzzerPin 7
 
-#define redledPin 2
-#define yellowledPin 3
-#define greenledPin 4
+#define redledPin 8
+#define yellowledPin 9
+#define greenledPin 3
 
-// Constructor for SH1106 128x64 SPI display
-U8G2_SSD1306_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ csPin, /* dc=*/ dcPin, /* reset=*/ resPin);
+char buffer[30];
+const char outofseconds[] PROGMEM = " out of 3 seconds";
+
+float RL = 10000.0; 
+float R0 = 41000.0;  
+float a = -0.48;    
+float b = 1.58;
+
+U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0);
 
 void setup() {
+  Serial.begin(9600);
   pinMode(buttonPin, INPUT);
   pinMode(buzzerPin,OUTPUT);
   pinMode(redledPin,OUTPUT);
   pinMode(yellowledPin,OUTPUT);
   pinMode(greenledPin, OUTPUT);
+  delay(1000);
+
   tone(buzzerPin,750,1000);
+
   u8g2.begin();
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_9x15_t_cyrillic);  // Choose a suitable font
-  u8g2.drawStr(0,10,"Warming up.");
-  u8g2.sendBuffer();
-  delay(20000);
-  u8g2.clearBuffer();
-  u8g2.drawStr(0, 10, "Waiting...");
-  u8g2.sendBuffer();
+  u8g2.firstPage();
+  do{
+    
+  } while(u8g2.nextPage());
+  u8g2.setFont(u8g2_font_6x12_tf);
+
+  for(int i = 0; i<128; i++){
+    u8g2.firstPage();
+    do{
+      u8g2.drawFrame(0, 40, 128, 10); 
+      u8g2.setFont(u8g2_font_6x12_tf);
+      u8g2.drawStr(30,35,"Preparing..."); 
+      u8g2.drawBox(0, 40, i, 10);
+    } while(u8g2.nextPage());
+    delay(1000);
+  }
+  delay(2000);
+u8g2.firstPage();
+    do{
+      u8g2.drawFrame(10, 10, 110, 40);
+      u8g2.drawStr(25, 35, "Ready to test.");
+    } while(u8g2.nextPage());
 }
-void loop() {                  // Transfer internal memory to the display
+ 
+void loop() {       
   if(digitalRead(buttonPin) == HIGH){
+
     stopLEDs();
     digitalWrite(yellowledPin, HIGH);
     int counter = 1;
@@ -45,16 +69,24 @@ void loop() {                  // Transfer internal memory to the display
       counter++;
       delay(1000);
     }
-    float avgAlco = ArrAvg(measurements, 3);
-    float alcovalue = ((avgAlco / 10) * (5.0 / 1024.0))*0.67;
-    u8g2.clearBuffer();
-    u8g2.drawStr(0, 10, String(measurements[0]).c_str());
-    u8g2.drawStr(0, 25, String(measurements[1]).c_str());
-    u8g2.drawStr(0, 40, String(measurements[2]).c_str());
-    u8g2.drawStr(0, 55, String(alcovalue).c_str());
-    u8g2.sendBuffer();
+    int analogValue = ArrMax(measurements,3);
+  	float RS = RL * (1023.0 - analogValue) / analogValue;
+    float ratio = RS / R0;
+    float alcoholPPM = pow(10, a * log10(ratio) + b);
+    float BrAC = alcoholPPM / 500.0;
+    float BAC = BrAC * 0.21 * 10;
+
+    Serial.println(BAC);
+    Serial.println(analogValue);
+    u8g2.firstPage();
+    do{
+      u8g2.setFont(u8g2_font_6x12_tf);
+      u8g2.drawFrame(10, 10, 110, 40);
+      u8g2.setCursor(55, 35);
+      u8g2.print(BAC);
+    } while(u8g2.nextPage());
     digitalWrite(yellowledPin,LOW);
-    if(alcovalue<0.5){
+    if(BAC<0.5){
       digitalWrite(greenledPin,HIGH);
       tone(buzzerPin,1000,1000);
     }
@@ -76,22 +108,23 @@ void stopLEDs(){
   digitalWrite(greenledPin,LOW);
 }
 void printTimer(int seconds){
-  u8g2.clearBuffer();
-  u8g2.drawStr(0, 10, String(seconds).c_str());
-  u8g2.drawStr(5, 10, " out of 3 seconds");
-  u8g2.sendBuffer();
+  u8g2.firstPage();
+  do{
+    snprintf(buffer,sizeof(buffer),"%d",seconds);
+    u8g2.drawFrame(10, 40, 100, 10); 
+    u8g2.drawBox(10, 40, seconds * 33, 10);
+    u8g2.drawStr(5, 20, buffer);
+    strcpy_P(buffer,outofseconds);
+    u8g2.drawStr(10, 20, buffer);
+  } while(u8g2.nextPage());
+
 }
-void printResults(int orgvalue, float idk){
-  u8g2.clearBuffer();
-  u8g2.drawStr(0 ,20, String(idk).c_str());
-  u8g2.drawStr(30, 20, "mgL");
-  u8g2.drawStr(0, 40, String(orgvalue).c_str());
-  u8g2.sendBuffer();
-}
-float ArrAvg(int a[], int n){
-  int sum = 0;
+float ArrMax(int a[], int n){
+  int max = a[0];
   for(int i = 0; i < n;i++){
-    sum += a[i];
+    if(a[i]>max){
+      max = a[i];
+    }
   }
-  return (float)sum/n;
+  return max;
 }
